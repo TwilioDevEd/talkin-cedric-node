93d667b473b14c57e41ae1e4cdc9add90a280d98
Alexis Crespo <alexis-2cab@hotmail.com>
2021-10-28 08:41:28 -0500

    Update default behavior to production environment settings

--- server.js
+++ server.js
@@ -81,25 +81,12 @@ app.ws("/media", (ws, req) => {
   });
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
-}
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  console.trace(err);
   res.status(err.status || 500);
-  res.render('error', {
+  res.send({
     message: err.message,
     error: {},
   });
